6acf150a50b52c027a2e737cce4bfa6e9b4e1d78
Chris Johnson <49479599+workeffortwaste@users.noreply.github.com>
2022-11-16 10:16:35 +0000

    Fixes for the new SERP layout

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 window.addEventListener('load', (event) => {
-  for (const item of document.querySelectorAll('[data-pcu]')) {
+  for (const item of document.querySelectorAll('[aria-label="Ads"] a')) {
     if (item.href.includes('aclk?')) {
-      item.children[1].setAttribute('style', 'text-decoration:line-through;')
+      item.setAttribute('style', 'text-decoration:line-through;')
       continue
     }
     item.setAttribute('data-rw', item.href)
     item.setAttribute('data-ved', '')
-    item.children[2].getElementsByTagName('span')[0].innerHTML = 'Nt<span style="padding:0 5px">·</span>'
-  }
-  for (const item of document.querySelectorAll('[aria-label="Ads"] a')) {
-    if (item.href.includes('aclk?')) {
-      item.setAttribute('style', 'text-decoration:line-through;')
+
+    if (item.children[1]) {
+      item.children[1].getElementsByTagName('span')[0].innerHTML = 'Nt<span style="padding:0 5px">·</span>'
     }
   }
 })
